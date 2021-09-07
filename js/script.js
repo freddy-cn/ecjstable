babb0932184ce212d032a3ded4902168cb752630
freddy <albertcn12@gmail.com>
2021-09-07 17:20:21 -0500

    added Search feature

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let pages = [];
+let timeout = undefined
 
 const loadTable = (data,options) => {
     paginate(data,options);
@@ -7,6 +8,7 @@ const loadTable = (data,options) => {
 }
 
 const paginate = (data,options) => {
+    pages = [];
     let pageNumber = 0;
 
     data.forEach((row, index) => {
@@ -25,15 +27,17 @@ const paginate = (data,options) => {
 
 const createLinks = (options) =>{
     const linksContainer = document.querySelector('#links-container');
+    linksContainer.innerHTML = "";
     for(let i = 0; i < pages.length; i++){
         const linkButton = `<button onclick="populateTable(${i})" class="table-link">${i+1}</button>`;
         linksContainer.innerHTML += linkButton;
     }
 }
 
-function populateTable(page){
+const populateTable = (page) => {
     const tableBody = document.querySelector('#table-body');
     tableBody.innerHTML = '';
+    if(pages.length < 1) return;
     pages[page].forEach(user => {
         const tr = document.createElement('tr');
         Object.values(user).forEach(field => {
@@ -44,3 +48,25 @@ function populateTable(page){
         tableBody.appendChild(tr);
     });
 }
+
+const filter = (evt) =>{
+    
+    if (timeout) clearTimeout(timeout);
+     
+    timeout = setTimeout(() => {
+        let searchWord = evt.target.value;
+        let filteredData = DATA.filter(row => {
+            let wordCount = 0;
+            Object.values(row).forEach(field => {
+                let fieldText = String(field).toLowerCase();
+                if(fieldText.includes(String(searchWord).toLowerCase())){
+                    wordCount++;
+                }
+            });
+            if(wordCount > 0) return true;
+        });
+        
+        loadTable(filteredData,options)
+    }, 300); 
+}
+
