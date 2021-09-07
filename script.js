2060578e4defa37b517369afb98a59b168650bee
freddy <albertcn12@gmail.com>
2021-09-07 14:31:58 -0500

    Project started

---
+++ script.js
@@ -0,0 +1,281 @@
+let DATA = [
+    {
+      "id": 1,
+      "first_name": "Lannie",
+      "last_name": "Garry",
+      "email": "lgarry0@wikispaces.com",
+      "gender": "Genderqueer"
+    }, {
+      "id": 2,
+      "first_name": "Fayth",
+      "last_name": "Dwyer",
+      "email": "fdwyer1@mapy.cz",
+      "gender": "Bigender"
+    }, {
+      "id": 3,
+      "first_name": "Georgie",
+      "last_name": "Zanotti",
+      "email": "gzanotti2@blogger.com",
+      "gender": "Polygender"
+    }, {
+      "id": 4,
+      "first_name": "Cristin",
+      "last_name": "Skeldinge",
+      "email": "cskeldinge3@msu.edu",
+      "gender": "Bigender"
+    }, {
+      "id": 5,
+      "first_name": "Temp",
+      "last_name": "Backhouse",
+      "email": "tbackhouse4@jugem.jp",
+      "gender": "Female"
+    }, {
+      "id": 6,
+      "first_name": "Susanna",
+      "last_name": "Rothschild",
+      "email": "srothschild5@vk.com",
+      "gender": "Male"
+    }, {
+      "id": 7,
+      "first_name": "Dorthea",
+      "last_name": "Stokell",
+      "email": "dstokell6@symantec.com",
+      "gender": "Non-binary"
+    }, {
+      "id": 8,
+      "first_name": "Larine",
+      "last_name": "Rouch",
+      "email": "lrouch7@dailymotion.com",
+      "gender": "Female"
+    }, {
+      "id": 9,
+      "first_name": "Andie",
+      "last_name": "McPhaden",
+      "email": "amcphaden8@opensource.org",
+      "gender": "Non-binary"
+    }, {
+      "id": 10,
+      "first_name": "Wylma",
+      "last_name": "Cowherd",
+      "email": "wcowherd9@skyrock.com",
+      "gender": "Genderfluid"
+    }, {
+      "id": 11,
+      "first_name": "Rayner",
+      "last_name": "Conkie",
+      "email": "rconkiea@dailymotion.com",
+      "gender": "Female"
+    }, {
+      "id": 12,
+      "first_name": "Carmella",
+      "last_name": "Rosie",
+      "email": "crosieb@icq.com",
+      "gender": "Genderqueer"
+    }, {
+      "id": 13,
+      "first_name": "Conney",
+      "last_name": "Whetson",
+      "email": "cwhetsonc@51.la",
+      "gender": "Polygender"
+    }, {
+      "id": 14,
+      "first_name": "Cathrin",
+      "last_name": "Klimek",
+      "email": "cklimekd@goo.ne.jp",
+      "gender": "Polygender"
+    }, {
+      "id": 15,
+      "first_name": "Claus",
+      "last_name": "MacCorkell",
+      "email": "cmaccorkelle@netvibes.com",
+      "gender": "Polygender"
+    }, {
+      "id": 16,
+      "first_name": "Barbee",
+      "last_name": "Itzkovitch",
+      "email": "bitzkovitchf@cbc.ca",
+      "gender": "Female"
+    }, {
+      "id": 17,
+      "first_name": "Delcine",
+      "last_name": "Bartholomew",
+      "email": "dbartholomewg@dedecms.com",
+      "gender": "Male"
+    }, {
+      "id": 18,
+      "first_name": "Sayre",
+      "last_name": "Schuchmacher",
+      "email": "sschuchmacherh@nydailynews.com",
+      "gender": "Genderfluid"
+    }, {
+      "id": 19,
+      "first_name": "Lorant",
+      "last_name": "Chartre",
+      "email": "lchartrei@comcast.net",
+      "gender": "Male"
+    }, {
+      "id": 20,
+      "first_name": "Creight",
+      "last_name": "Eager",
+      "email": "ceagerj@walmart.com",
+      "gender": "Male"
+    }, {
+      "id": 21,
+      "first_name": "El",
+      "last_name": "Soigne",
+      "email": "esoignek@icio.us",
+      "gender": "Non-binary"
+    }, {
+      "id": 22,
+      "first_name": "Ambrosio",
+      "last_name": "Heater",
+      "email": "aheaterl@aboutads.info",
+      "gender": "Genderfluid"
+    }, {
+      "id": 23,
+      "first_name": "Galven",
+      "last_name": "Cambling",
+      "email": "gcamblingm@google.cn",
+      "gender": "Agender"
+    }, {
+      "id": 24,
+      "first_name": "Tonnie",
+      "last_name": "Pestricke",
+      "email": "tpestricken@oracle.com",
+      "gender": "Agender"
+    }, {
+      "id": 25,
+      "first_name": "Mae",
+      "last_name": "Connochie",
+      "email": "mconnochieo@kickstarter.com",
+      "gender": "Female"
+    }, {
+      "id": 26,
+      "first_name": "Hayden",
+      "last_name": "Piwell",
+      "email": "hpiwellp@w3.org",
+      "gender": "Female"
+    }, {
+      "id": 27,
+      "first_name": "Shoshanna",
+      "last_name": "McTrustam",
+      "email": "smctrustamq@creativecommons.org",
+      "gender": "Genderfluid"
+    }, {
+      "id": 28,
+      "first_name": "Consalve",
+      "last_name": "Brumby",
+      "email": "cbrumbyr@gnu.org",
+      "gender": "Genderfluid"
+    }, {
+      "id": 29,
+      "first_name": "Ruddy",
+      "last_name": "Seint",
+      "email": "rseints@edublogs.org",
+      "gender": "Male"
+    }, {
+      "id": 30,
+      "first_name": "Cari",
+      "last_name": "Shieber",
+      "email": "cshiebert@cbslocal.com",
+      "gender": "Agender"
+    }, {
+      "id": 31,
+      "first_name": "Jodi",
+      "last_name": "Dogerty",
+      "email": "jdogertyu@exblog.jp",
+      "gender": "Polygender"
+    }, {
+      "id": 32,
+      "first_name": "Gino",
+      "last_name": "Martello",
+      "email": "gmartellov@com.com",
+      "gender": "Bigender"
+    }, {
+      "id": 33,
+      "first_name": "Peri",
+      "last_name": "Rimell",
+      "email": "primellw@storify.com",
+      "gender": "Genderfluid"
+    }, {
+      "id": 34,
+      "first_name": "Gennifer",
+      "last_name": "Belderson",
+      "email": "gbeldersonx@moonfruit.com",
+      "gender": "Genderfluid"
+    }, {
+      "id": 35,
+      "first_name": "Elset",
+      "last_name": "Huddart",
+      "email": "ehuddarty@sourceforge.net",
+      "gender": "Non-binary"
+    }, {
+      "id": 36,
+      "first_name": "Ileane",
+      "last_name": "Emanuel",
+      "email": "iemanuelz@wikispaces.com",
+      "gender": "Male"
+    }, {
+      "id": 37,
+      "first_name": "Marven",
+      "last_name": "Giraudoux",
+      "email": "mgiraudoux10@canalblog.com",
+      "gender": "Bigender"
+    }, {
+      "id": 38,
+      "first_name": "Hansiain",
+      "last_name": "Matovic",
+      "email": "hmatovic11@imgur.com",
+      "gender": "Agender"
+    }, {
+      "id": 39,
+      "first_name": "Petey",
+      "last_name": "Cotte",
+      "email": "pcotte12@jugem.jp",
+      "gender": "Female"
+    }, {
+      "id": 40,
+      "first_name": "Yard",
+      "last_name": "D'Agostini",
+      "email": "ydagostini13@army.mil",
+      "gender": "Bigender"
+    }
+];
+const TOTAL_ROWS = DATA.length;
+let pages = [];
+const PER_PAGE = 5;
+const CURRENT_PAGE = 1;
+
+function segment(data){
+    let pageNumber = 0;
+    let currentPageSize = 0;
+    DATA.forEach((row, index) => {
+        if(!pages[pageNumber]){
+            pages[pageNumber] = [];
+        }
+
+        if(currentPageSize <= PER_PAGE){
+            pages[pageNumber].push(row);
+            currentPageSize++;
+        }else{
+            pageNumber++;
+            currentPageSize = 0;
+        }
+    });
+}
+
+segment();
+
+function loadData(){
+    const tableBody = document.querySelector('#table-body');
+    pages[CURRENT_PAGE].forEach(user => {
+        const tr = document.createElement('tr');
+        Object.values(user).forEach(field => {
+            const td = document.createElement('td');
+            td.innerHTML = field;
+            tr.appendChild(td);
+        });
+        tableBody.appendChild(tr);
+    });
+}
+loadData();
